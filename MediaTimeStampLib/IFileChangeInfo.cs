74905753810ec99e2049675697459e1e25197d61
Mark Olbert <mark@arcabama.com>
2022-03-01 13:45:23 -0800

    enhance interface to enable cross-thread updating of IFileChangeInfo objects and collections

--- MediaTimeStampLib/IFileChangeInfo.cs
+++ MediaTimeStampLib/IFileChangeInfo.cs
@@ -2,10 +2,13 @@
 
 public interface IFileChangeInfo
 {
+    bool FileExists { get; }
     string FilePath { get; set; }
-    DateTime DateCreated { get; }
+    DateTime? DateCreated { get; }
     DateTime? DateTaken { get; set; }
     ScanStatus ScanStatus { get; set; }
     string? ExtractorName { get; set; }
     List<TagInfo> Tags { get; }
+
+    void DoAction( Action<IFileChangeInfo> action );
 }
---
+++ MediaTimeStampLib/services/IMultiThreadCollection.cs
@@ -0,0 +1,6 @@
+﻿namespace J4JSoftware.ExifTSUpdater;
+
+public interface IMultiThreadCollection<T> : IList<T>
+{
+    void DoAction( Action<ICollection<T>> action );
+}
